da26782cfd209c213bce56546280f519fd977815
Kangwei Xiao <cocolonel.x@gmail.com>
2015-09-14 21:49:15 +0800

    Tutor management app models partially finished

--- HR/models.py
+++ HR/models.py
@@ -2,6 +2,49 @@ from django.db import models
 
 # Create your models here.
 class Tutor(models.Model):
-    name = models.CharField(max_length=200)
-    
-    pub_date = models.DateTimeField('date published')
+    name = models.CharField(max_length=100)
+    gender = (
+        (F, 'Female'),
+        (M, 'Male'),
+    )
+    subject_teach = models.ManyToManyField(Subject)
+    education = models.ManyToManyField(Education,through='Graduation')
+    region = (
+        (E, 'East'),
+        (W, 'West'),
+        (C, 'Central'),
+        (N, 'North'),
+    )
+    time = (
+        (M, 'Morning'),
+        (A, 'Afternoon'),
+        (E, 'Evening'),
+    )
+
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    level = (
+        (P, 'Primary School'),
+        (O, 'O-level'),
+        (A, 'A-level'),
+    )
+
+class Education(models.Model)
+    name = models.CharField(max_length=100)
+    level = (
+        (J, 'Junior college'),
+        (P, 'Polytechnic'),
+        (U, 'University'),
+    )
+
+    def __str__(self):
+        return self.name
+
+class Graduation(models.Model)
+    tutor = models.ForeignKey(Tutor)
+    education = models.ForeignKey(Education)
+    date = models.DateTimeField('Date of graduation')
+    description = models.CharField(max_length=500)
