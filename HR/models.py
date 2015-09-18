427cf142122c6ea59685d86efe08772d545ba709
XiaoKangwei <cocolonel.x@gmail.com>
2015-09-18 21:31:21 +0800

    refactor

--- HR/models.py
+++ HR/models.py
@@ -2,46 +2,48 @@ from django.db import models
 
 # Create your models here.
 class Subject(models.Model):
-    title = models.CharField(max_length=100)
-    level = (
+    LEVEL = (
         ('P', 'Primary School'),
         ('O', 'O-level'),
         ('A', 'A-level'),
     )
+    title = models.CharField(max_length=100)
     def __str__(self):
         return self.title
 
 class Education(models.Model):
-    name = models.CharField(max_length=100)
-    level = (
+    LEVEL = (
         ('J', 'Junior college'),
         ('P', 'Polytechnic'),
         ('U', 'University'),
     )
+    name = models.CharField(max_length=100)
+    
 
     def __str__(self):
         return self.name
 
 class Tutor(models.Model):
-    name = models.CharField(max_length=100)
-    gender = (
-        ('F', 'Female'),
-        ('M', 'Male'),
-    )
-    subject_teach = models.ManyToManyField(Subject)
-    education = models.ManyToManyField(Education,through='Graduation')
-    region = (
+    REGION = (
         ('E', 'East'),
         ('W', 'West'),
         ('C', 'Central'),
         ('N', 'North'),
     )
-    time = (
+    TIME = (
         ('M', 'Morning'),
         ('A', 'Afternoon'),
         ('E', 'Evening'),
     )
-
+    GENDER = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+    
+    name = models.CharField(max_length=100)
+    subject_teach = models.ManyToManyField(Subject)
+    education = models.ManyToManyField(Education,through='Graduation')
+    
     def __str__(self):
         return self.name
 
